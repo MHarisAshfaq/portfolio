8dda952c9f6048272e010b76e4e9959a96dbc84e
xlmb03 <xlmb03@xlmb03s-MacBook-Pro.local>
2023-12-22 00:42:51 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,245 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          {/* Get started by editing&nbsp; */}
+          <code className="font-mono font-bold">
+            {" "}
+            Muhammad Haris (Full Stack Dev)
+          </code>
+        </p>
+      </div>
+      <img
+        className="rounded-full"
+        src="profile.png"
+        alt="profile"
+        width={"100%"}
+        height={"10%"}
+      />
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] my-10">
+        <p className={`m-0 max-w-[30ch] text-sm`}>
+          Hello, I'm Muhammad Haris, a passionate and dedicated Full Stack
+          Developer with over 3 years of experience in MERN Stack. Currently
+          based in Lahore, Pakistan.
+        </p>
+      </div>
+      <div className="my-2">
+        <h3 className="mb-4">Languages and Tools:</h3>
+        <p className="grid grid-rows-4 grid-flow-col gap-4">
+          {" "}
+          <a href="https://getbootstrap.com" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
+              alt="bootstrap"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a
+            href="https://www.w3schools.com/css/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+              alt="css3"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://www.docker.com/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg"
+              alt="docker"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://expressjs.com" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+              alt="express"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+              alt="git"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+              alt="html5"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+              alt="javascript"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://jestjs.io" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg"
+              alt="jest"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://www.linux.org/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg"
+              alt="linux"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+              alt="mongodb"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+              alt="nodejs"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+              alt="react"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a href="https://redux.js.org" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
+              alt="redux"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+          <a
+            href="https://www.typescriptlang.org/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
+              alt="typescript"
+              width="40"
+              height="40"
+            />{" "}
+          </a>{" "}
+        </p>
+      </div>
+      {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore starter templates for Next.js.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div> */}
+    </main>
+  );
+}
