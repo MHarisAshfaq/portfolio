68ce0d7d7d25f9995882b8d8b82ec9f79c622b99
MHarisAshfaq <muhammadharisashfaq@gmail.com>
2023-12-23 01:36:40 +0500

    next.config and workflowfile updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async generateStaticPaths() {
-    // Generate static paths as needed
-    return {
-      "/": { page: "/" },
-      // Add other pages as needed
-    };
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
